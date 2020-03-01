9a643ee5dc01d6a819469927a38c631ce10b0793
David Colón <david@davidiq.com>
2020-02-29 23:26:14 -0500

    Code updates and 3.3 compatibility

--- acp/forumbanners_info.php
+++ acp/forumbanners_info.php
@@ -1,28 +1,27 @@
 <?php
 /**
-* Forum Banner extension for the phpBB Forum Software package.
-*
-* @copyright (c) 2015 DavidIQ.com
-* @license GNU General Public License, version 2 (GPL-2.0)
-*/
+ * Forum Banner extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 DavidIQ.com
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ */
 
 namespace davidiq\ForumBanners\acp;
 
 class forumbanners_info
 {
-	function module()
-	{
-		return array(
-			'filename'	=> '\davidiq\ForumBanners\acp\forumbanners_module',
-			'title'		=> 'ACP_FORUMBANNER_IMAGES',
-			'version'	=> '1.0.0',
-			'modes'		=> array(
-				'main'		=> array(
-						'title' => 'ACP_FORUMBANNER_IMAGES',
-						'auth' => 'ext_davidiq/ForumBanners && acl_a_fauth',
-						'cat' 	=> array('ACP_CAT_FORUMBANNERS'),
-				),
-			),
-		);
-	}
+    function module()
+    {
+        return [
+            'filename' => '\davidiq\ForumBanners\acp\forumbanners_module',
+            'title' => 'ACP_FORUMBANNER_IMAGES',
+            'modes' => [
+                'main' => [
+                    'title' => 'ACP_FORUMBANNER_IMAGES',
+                    'auth' => 'ext_davidiq/ForumBanners && acl_a_fauth',
+                    'cat' => ['ACP_CAT_FORUMBANNERS'],
+                ],
+            ],
+        ];
+    }
 }
